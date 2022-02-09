e9011e4921bf49d51c0c0e4c2ff31ffbccd3bb8c
Rany-Touffaha <rany.touffaha@kcl.ac.uk>
2022-02-09 14:58:39 +0000

    Add ParseJSON function to TFL

--- app/src/main/java/com/example/backstreet_cycles/Tfl.kt
+++ app/src/main/java/com/example/backstreet_cycles/Tfl.kt
@@ -1,7 +1,29 @@
 package com.example.backstreet_cycles
 
+import android.util.Log
+import com.example.backstreet_cycles.data.Dock
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+
 class Tfl {
 
+    private val listType = Types.newParameterizedType(
+        List::class.java, Dock::class.java
+    )
 
+    fun parseJSON (url: String){
+        val moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<List<Dock>> = moshi.adapter(listType)
+        val dockData = adapter.fromJson("@string/tfl_url")
+
+        for (dock in dockData?: emptyList()){
+            Log.i("Rany", "${dock.lat}")
+        }
+    }
+
+//    fun getDockFromJSON(): Dock{
+//
+//    }
 
 }
