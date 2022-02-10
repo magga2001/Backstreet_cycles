dc183e7a447b7d72e50689ad6689c806452aeb33
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-02-10 06:35:26 +0000

    -Create a tfl api class -Create Application class -Add a timer to Main activity class

--- app/src/main/java/com/example/backstreet_cycles/MainActivity.kt
+++ app/src/main/java/com/example/backstreet_cycles/MainActivity.kt
@@ -2,10 +2,66 @@ package com.example.backstreet_cycles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    var mapView: MapView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkLoading()
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+    }
+
+    private fun checkLoading() {
+
+        Log.i("COUNT_DOWN", "RUN")
+        object: CountDownTimer(3000,1000)
+        {
+            override fun onTick(p0: Long) {
+                Log.i("COUNT_DOWN", "OnTickRUn")
+            }
+
+            override fun onFinish() {
+                Log.i("COUNT_DOWN", "OnFinishRUn")
+                if(Tfl.isLoaded)
+                {
+                    Log.i("Dock_station", Tfl.docks.toString())
+                    //THEN GO TO MAIN ACTIVITY
+                }
+                else
+                {
+                    checkLoading()
+                }
+
+            }
+        }.start()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 }
--- app/src/main/java/com/example/backstreet_cycles/data/Dock.kt
+++ app/src/main/java/com/example/backstreet_cycles/data/Dock.kt
@@ -1,7 +1,5 @@
 package com.example.backstreet_cycles.data
 
-import com.google.gson.annotations.SerializedName
-
 data class Dock(
     val id : String,
     val name: String,
