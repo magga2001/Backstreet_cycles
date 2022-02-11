38eea78ef1da26a2eb567a1dc3a9ef2b939d944c
Maya Janbey <msjanbey@gmail.com>
2022-02-11 11:49:40 +0000

    Remove redundant import statements

--- app/src/main/java/com/example/backstreet_cycles/LogInActivity.kt
+++ app/src/main/java/com/example/backstreet_cycles/LogInActivity.kt
@@ -3,23 +3,13 @@ package com.example.backstreet_cycles
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-
-import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
 import kotlinx.android.synthetic.main.activity_log_in.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
-import kotlinx.android.synthetic.main.activity_log_in.buttonLogin
-import kotlinx.android.synthetic.main.activity_log_in.et_password
-import kotlinx.android.synthetic.main.activity_sign_up.et_email
+
 
 class LogInActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    private var customToken: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +27,6 @@ class LogInActivity : AppCompatActivity() {
                 et_password.text.trim().toString()
             )
         }
-
-
-//            findViewById<Button>(R.id.button_register).setOnClickListener {
-//            startActivity(Intent(this@LogInActivity, SignUpActivity::class.java))
-//        }
 
     }
 
