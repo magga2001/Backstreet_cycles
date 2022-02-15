65049c6694441083107bce1e9bce370f804f86c2
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-02-15 14:16:23 +0000

    - Current location of the user - Displaying dock station on the map - Implement docks within radius - Implement permission pop up for user

--- app/src/main/java/com/example/backstreet_cycles/MainActivity.kt
+++ app/src/main/java/com/example/backstreet_cycles/MainActivity.kt
@@ -1,26 +1,64 @@
 package com.example.backstreet_cycles
 
-import androidx.appcompat.app.AppCompatActivity
+//---------------------------------
+
+//import android.R
+import android.content.Context
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.Canvas
+import androidx.annotation.DrawableRes
+import android.graphics.drawable.Drawable
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.lifecycleScope
-import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
+import com.example.backstreet_cycles.DTO.Dock
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.LocationUpdate
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
 
     var mapView: MapView? = null
+    private var permissionsManager: PermissionsManager? = null
+    private var mapboxMap: MapboxMap? = null
+    private var locationComponent: LocationComponent? = null
+    private var locationUpdate: LocationUpdate? = null
+    var docks= Tfl.docks
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
 
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
+        //mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
 
         lifecycleScope.launch {Log.i("Retrieve one data", Tfl.readDock("BikePoints_617").toString()) }
-        Log.i("Retrieve data", Tfl.docks.toString())
+        Log.i("Retrieve data", Tfl.docks.size.toString())
     }
 
     override fun onStart() {
@@ -41,5 +79,148 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+//        mapboxMap.addOnMapClickListener(this)
+        mapboxMap.setStyle(
+            Style.MAPBOX_STREETS
+        ) { style ->
+            enableLocationComponent(style)
+            displayingDocks(mapboxMap,style)
+        }
+    }
+
+    private fun displayingDocks(mapboxMap: MapboxMap, loadedMapStyle: Style)
+    {
+        val symbolManager = mapView?.let { SymbolManager(it, mapboxMap, loadedMapStyle) }
+        symbolManager?.iconAllowOverlap = true
+        val bitmap = bitmapFromDrawableRes(this,R.drawable.marker_map) as Bitmap
+        loadedMapStyle.addImage("myMarker", Bitmap.createScaledBitmap(bitmap, 10, 10, false))
+        for(dock in docks)
+        {
+            symbolManager?.create(
+                SymbolOptions()
+                    .withLatLng(LatLng(dock.lat, dock.lon))
+                    .withIconImage("myMarker")
+            )
+        }
+    }
+
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+// copying drawable object to not manipulate on the same reference
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) { // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            // Enable the most basic pulsing styling by ONLY using
+            // the `.pulseEnabled()` method
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .pulseEnabled(true)
+                .build()
+
+            // Get an instance of the component
+            //val locationComponent: LocationComponent? = mapboxMap?.locationComponent
+            locationComponent = mapboxMap?.locationComponent
+
+
+            // Activate with options
+            locationComponent?.activateLocationComponent(
+                LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                    .locationComponentOptions(customLocationComponentOptions)
+                    .build()
+            )
+
+            // Enable to make component visible
+            locationComponent?.isLocationComponentEnabled = true
+
+            // Set the component's camera mode
+            locationComponent?.cameraMode = CameraMode.TRACKING
+
+            // Set the component's render mode
+            locationComponent?.renderMode = RenderMode.COMPASS
+
+            Log.i("Retrieve closest dock", getRadiusDocks(0.01).size.toString())
+            //Update to current location
+            //locationComponent?.forceLocationUpdate(LocationUpdate.Builder().build())
+
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager?.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String?>?) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            mapboxMap?.getStyle { style -> enableLocationComponent(style) }
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG)
+                .show()
+            finish()
+        }
+    }
+
+    private fun getCurrentLocation(): Location?
+    {
+        //Can call lat and lon from this function
+        return locationComponent!!.lastKnownLocation
+    }
+
+    private fun getRadiusDocks(radius: Double): List<Dock>
+    {
+        val currentLat = getCurrentLocation()?.latitude as Double
+        val currentLon = getCurrentLocation()?.longitude as Double
+
+        val closestDocks = mutableListOf<Dock>()
+
+        for(dock in docks)
+        {
+            val dockLat = dock.lat
+            val dockLon = dock.lon
+
+            if(abs(dockLat - currentLat) <= radius && abs(dockLon - currentLon) <= radius)
+            {
+                closestDocks.add(dock)
+            }
+        }
+
+        return closestDocks
     }
 }
