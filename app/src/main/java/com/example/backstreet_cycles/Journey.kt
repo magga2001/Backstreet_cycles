6c1657bda4f12c0fb7665a4cb8007f3d6a721a6a
Vandad Vafai <vafai.vandad@gmail.com>
2022-02-16 18:51:45 +0000

    Fixing all the activities to implement logic

--- app/src/main/java/com/example/backstreet_cycles/Journey.kt
+++ app/src/main/java/com/example/backstreet_cycles/Journey.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_journey.*
 
 
 class Journey : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journey)
-
-        var goButton = findViewById<Button>(R.id.goButton)
+        
         goButton.setOnClickListener {
             val intent = Intent(this, Journey_on_map::class.java)
             startActivity(intent)
         }
     }
+
 }
--- app/src/main/java/com/example/backstreet_cycles/Navigation.kt
+++ app/src/main/java/com/example/backstreet_cycles/Navigation.kt
@@ -7,5 +7,7 @@ class Navigation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+
     }
 }
