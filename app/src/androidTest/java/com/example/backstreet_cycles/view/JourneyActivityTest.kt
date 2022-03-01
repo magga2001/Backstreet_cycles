62e3f3f0276909cff4e18561e47c9f709af8a8dd
Vandad Vafai <vafai.vandad@gmail.com>
2022-03-01 15:05:43 +0000

    begin ui tests

---
+++ app/src/androidTest/java/com/example/backstreet_cycles/view/JourneyActivityTest.kt
@@ -0,0 +1,21 @@
+package com.example.backstreet_cycles.view
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.example.backstreet_cycles.R
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class JourneyActivityTest{
+    @Test
+    fun test_journey_activity_is_visible(){
+        val activityScenario= ActivityScenario.launch(JourneyActivity::class.java)
+        onView(withId(R.id.journeyActivity)).check(matches(isDisplayed()))
+    }
+}
