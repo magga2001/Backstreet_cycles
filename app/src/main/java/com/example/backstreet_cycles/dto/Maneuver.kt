e7fb272e8828e32c4daac4d630f5132f9e32ecb4
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-03-01 03:31:26 +0000

    - Add distance variable into Maneuver dto

--- app/src/main/java/com/example/backstreet_cycles/dto/Maneuver.kt
+++ app/src/main/java/com/example/backstreet_cycles/dto/Maneuver.kt
@@ -3,5 +3,6 @@ package com.example.backstreet_cycles.dto
 data class Maneuver(
     val instruction: String,
     val type: String,
-    val modifier: String = ""
+    val modifier: String = "",
+    val distance: Int
 )
