63b0e079f3b33991ee19bc7a00b93f1920a39767
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2022-03-08 05:55:16 +0000

    - Delete Api and App file

--- app/src/main/java/com/example/backstreet_cycles/view/SplashScreenActivity.kt
+++ app/src/main/java/com/example/backstreet_cycles/view/SplashScreenActivity.kt
@@ -2,21 +2,13 @@ package com.example.backstreet_cycles.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.backstreet_cycles.Api
 import com.example.backstreet_cycles.PlanJourneyActivity
 import com.example.backstreet_cycles.R
-import com.example.backstreet_cycles.dto.Dock
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlin.concurrent.thread
 
 class SplashScreenActivity: AppCompatActivity() {
 
