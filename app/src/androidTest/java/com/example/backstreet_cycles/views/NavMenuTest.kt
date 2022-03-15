9f5016404830b19f4784fdb0e8af53f75b828555
Rany-Touffaha <rany.touffaha@kcl.ac.uk>
2022-03-15 18:04:39 +0000

    Add test setup for NavMenuTest and tests test_drawer_is_open and test_drawer_is_closed.

--- app/src/androidTest/java/com/example/backstreet_cycles/views/NavMenuTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/views/NavMenuTest.kt
@@ -1,15 +1,18 @@
 package com.example.backstreet_cycles.views
 
+import android.view.Gravity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.backstreet_cycles.R
 import kotlinx.android.synthetic.main.activity_homepage.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,10 +20,26 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class NavMenuTest {
 
-    @Test
-    fun test_navToggle_toNavMenu() {;
+    @Before
+    fun setUp() {
         ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nav_view)).perform(click())
+        onView(withContentDescription(R.string.open)).perform(click())
+    }
+
+    @Test
+    fun test_drawer_is_open(){
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_drawer_is_closed(){
+        onView(withContentDescription(R.string.close)).perform(click())
+        onView(withId(R.id.nav_view)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
+    }
+
+    @Test
+    fun test_navToggle_toNavMenu() {
+        onView(withId(R.id.profile)).check(matches(isDisplayed()))
         onView(withId(R.id.changePassword)).check(matches(isDisplayed()))
         onView(withId(R.id.about)).check(matches(isDisplayed()))
         onView(withId(R.id.help)).check(matches(isDisplayed()))
@@ -29,16 +48,12 @@ class NavMenuTest {
 
     @Test
     fun test_viewProfileButton_toEditProfileActivity() {
-        ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nav_view)).perform(click())
         onView(withId(R.id.profile)).perform(click())
         onView(withId(R.id.editUserProfile)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_changePassword_toChangeEmailOrPasswordActivity() {
-        ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nav_view)).perform(click())
         onView(withId(R.id.changePassword)).perform(click())
         onView(withId(R.id.changeEmailOrPassword)).check(matches(isDisplayed()))
     }
@@ -61,19 +76,15 @@ class NavMenuTest {
         onView(withId(R.id.helpActivity)).check(matches(isDisplayed()))
     }*/
 
-    @Test
-    fun test_logoutButton_toLoginActivity() {
-        ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nav_view)).perform(click())
-        onView(withId(R.id.logout)).perform(click())
-        onView(withId(R.id.logInActivity)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun test_logoutButton_toLoginActivity() {
+//        onView(withId(R.id.logout)).perform(click())
+//        onView(withId(R.id.logInActivity)).check(matches(isDisplayed()))
+//    }
 
-    @Test
-    fun test_backPress_toPreviousPage() {
-        ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nav_view)).perform(click())
-        pressBack()
-        onView(withId(R.id.homePageActivity)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun test_backPress_toPreviousPage() {
+//        onView(withId(R.string.))
+//        onView(withId(R.id.HomePageActivity)).check(matches(isDisplayed()))
+//    }
 }
