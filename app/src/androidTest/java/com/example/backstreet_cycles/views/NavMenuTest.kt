6156f2001605c388e846b6157ab5b61aa0f643f5
Shahmir Malik <k19029862@kcl.ac.uk>
2022-03-17 15:57:38 +0000

    Reinstate branch state

--- app/src/androidTest/java/com/example/backstreet_cycles/views/NavMenuTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/views/NavMenuTest.kt
@@ -1,16 +1,11 @@
 package com.example.backstreet_cycles.views
 
-import android.view.Gravity
 import android.app.Application
-import androidx.core.view.GravityCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.backstreet_cycles.R
@@ -20,10 +15,6 @@ import com.example.backstreet_cycles.viewModel.LogInRegisterViewModel
 import com.example.backstreet_cycles.viewModel.LoggedInViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_change_email_or_password.*
-import kotlinx.android.synthetic.main.activity_homepage.*
-import kotlinx.android.synthetic.main.nav_header.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -69,12 +60,13 @@ class NavMenuTest {
         onView(withId(R.id.editUserProfile)).check(matches(isDisplayed()))
     }
 
-    /*@Test
+    @Test
     fun test_logOutButton_toLogInActivity() {
         onView(withId(R.id.logout)).perform(click())
         onView(withId(R.id.logInActivity)).check(matches(isDisplayed()))
     }
-*/
+
+
     @Test
     fun test_changePassword_toChangeEmailOrPasswordActivity() {
         onView(withId(R.id.changePassword)).perform(click())
@@ -93,28 +85,21 @@ class NavMenuTest {
 
     @Test
     fun test_nav_showUserName(){
-
         onView(withId(R.id.user_name)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_nav_showUserEmail(){
-
         onView(withId(R.id.tv_email)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_nav_equalCurrentUserName(){
-
         val testUserName = FirebaseFirestore.getInstance().collection("users")
             .whereEqualTo("email", FirebaseAuth.getInstance().currentUser!!.email)
             .get().result.toObjects(Users::class.java)[0].firstName
-
         val textElement = "Hello: $testUserName"
-
         val testDisplayedName = getApplicationContext<Application>().getString(R.id.user_name)
-
-
         assert(testDisplayedName == textElement)
     }
 
