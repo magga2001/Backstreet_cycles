e4c3f1c8c39b9f5a8b7b3035edd88b9c127c8626
pkur3k <74495381+pkur3k@users.noreply.github.com>
2022-03-17 22:06:58 +0000

    Add tests to JourneyActivityTest (currently commented out due to the lack of proper set up)  - test_journey_activity_is_visible  - test_bottom_sheet_visible  - test_journey_overview_button_visible  - test_start_navigation_button_visible  - test_next_button_from_home_to_journey  - test_stops_text_field_displayed  - test_stops_recycling_view_displayed  - test_journey_overview_clicked  - test_start_navigation_clicked_goes_to_navigation_activity  - test_stop_clicked_set_navigation_displayed  - test_stop_clicked_images_displayed  - test_set_navigation_clicked

--- app/src/androidTest/java/com/example/backstreet_cycles/views/JourneyActivityTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/views/JourneyActivityTest.kt
@@ -9,6 +9,7 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.GrantPermissionRule
 import com.example.backstreet_cycles.R
 import com.example.backstreet_cycles.viewModel.LogInRegisterViewModel
 import org.junit.Assert.*
@@ -29,9 +30,87 @@ class JourneyActivityTest{
         onView(ViewMatchers.withContentDescription(R.string.open)).perform(ViewActions.click())
 
     }
-//    @Test
-//    fun test_journey_activity_is_visible(){
-//        val activityScenario= ActivityScenario.launch(JourneyActivity::class.java)
-//        onView(withId(R.id)).check(matches(isDisplayed()))
-//    }
+
+        /*@Before
+        fun setUp() {
+            GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            GrantPermissionRule.grant(android.Manifest.permission.ACCESS_NETWORK_STATE)
+            GrantPermissionRule.grant(android.Manifest.permission.INTERNET)
+            ActivityScenario.launch(HomePageActivity::class.java)
+            //onView(withId(R.id.addingBtn)).perform(click()).perform(typeText("Engineer"));
+
+            onView(withId(R.id.nextPageButton)).perform(ViewActions.click())
+        }*/
+
+       /* @Test
+        fun test_journey_activity_is_visible(){
+            onView(withId(R.id.journey_activity)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_bottom_sheet_visible(){
+            onView(withId(R.id.bottom_sheet_journey)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_journey_overview_button_visible(){
+            onView(withId(R.id.overview_journey)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_start_navigation_button_visible(){
+            onView(withId(R.id.start_navigation)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_next_button_from_home_to_journey(){
+            onView(withId(R.id.nextPageButton)).perform(ViewActions.click())
+            onView(withId(R.id.journey_activity)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_stops_text_field_displayed(){
+            onView(withId(R.id.stopsText)).check(matches(ViewMatchers.withText("stops:")))
+        }
+
+        @Test
+        fun test_stops_recycling_view_displayed(){
+            onView(withId(R.id.bottom_sheet_journey)).perform(ViewActions.click())
+            onView(withId(R.id.plan_journey_recycling_view)).check(matches(isDisplayed()))
+        }
+
+        *//*@Test
+        fun test_journey_overview_clicked(){
+
+            onView(withId(R.id.journey_activity)).check(matches(isDisplayed()))
+        }*//*
+
+        @Test
+        fun test_start_navigation_clicked_goes_to_navigation_activity(){
+            onView(withId(R.id.start_navigation)).perform(ViewActions.click())
+            onView(withId(R.id.navigationActivity)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_stop_clicked_set_navigation_displayed(){
+            onView(withId(R.id.button_expand)).perform(ViewActions.click())
+            onView(withId(R.id.setNav1)).check(matches(isDisplayed()))
+            onView(withId(R.id.setNav2)).check(matches(isDisplayed()))
+            onView(withId(R.id.setNav3)).check(matches(isDisplayed()))
+
+
+        }
+
+        @Test
+        fun test_stop_clicked_images_displayed(){
+            onView(withId(R.id.button_expand)).perform(ViewActions.click())
+            onView(withId(R.id.imageView13)).check(matches(isDisplayed()))
+            onView(withId(R.id.imageView14)).check(matches(isDisplayed()))
+        }
+
+        @Test
+        fun test_set_navigation_clicked(){
+
+            onView(withId(R.id.journey_activity)).check(matches(isDisplayed()))
+        }
 }
