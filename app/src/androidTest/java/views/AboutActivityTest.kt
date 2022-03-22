5ef7b2982c49005869cae65aaef43d4be4a0013c
Shahmir Malik <k19029862@kcl.ac.uk>
2022-03-22 12:10:04 +0000

    Fix about activity tests and login tests

--- app/src/androidTest/java/views/AboutActivityTest.kt
+++ app/src/androidTest/java/views/AboutActivityTest.kt
@@ -2,12 +2,9 @@ package views
 
 import android.app.Application
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
