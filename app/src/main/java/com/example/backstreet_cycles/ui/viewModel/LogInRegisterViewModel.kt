358b54a09743c3dde99d31d1ef68fb0842353b82
arjunkhanna10 <arjunkhanna945@gmail.com>
2022-03-24 16:25:14 +0000

    Commit Overview:-
    - Change LogInRegisterViewModel.kt so that it uses dependency injection.
    - Implement new LogInRegisterViewModel.kt in LogInActivity.kt and SignUpActivity.kt

--- app/src/main/java/com/example/backstreet_cycles/ui/viewModel/LogInRegisterViewModel.kt
+++ app/src/main/java/com/example/backstreet_cycles/ui/viewModel/LogInRegisterViewModel.kt
@@ -1,21 +1,32 @@
 package com.example.backstreet_cycles.ui.viewModel
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.backstreet_cycles.data.repository.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.internal.Contexts
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class LogInRegisterViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class LogInRegisterViewModel @Inject constructor(
+    @ApplicationContext applicationContext: Context
+): ViewModel() {
 
+    private val mApplication = Contexts.getApplication(applicationContext)
+    private val mContext = applicationContext
     private val userRepository: UserRepository
     private val mutableLiveData: MutableLiveData<FirebaseUser>
 
     init {
-        userRepository = UserRepository(application, Firebase.firestore, FirebaseAuth.getInstance())
+        userRepository = UserRepository(mApplication, Firebase.firestore, FirebaseAuth.getInstance())
         mutableLiveData = userRepository.getMutableLiveData()
     }
 
