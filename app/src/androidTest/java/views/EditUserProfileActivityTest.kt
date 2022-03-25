c63f4299f58e5a656e96cee97e31cb1aa1a704c4
arjunkhanna10 <arjunkhanna945@gmail.com>
2022-03-25 18:38:02 +0000

    Commit Overview:-
    - Implement new tests on EditUserProfileActivityTest.kt

--- app/src/androidTest/java/views/EditUserProfileActivityTest.kt
+++ app/src/androidTest/java/views/EditUserProfileActivityTest.kt
@@ -4,14 +4,15 @@ import android.app.Application
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.init
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.GrantPermissionRule
 import com.example.backstreet_cycles.R
@@ -84,9 +85,36 @@ class EditUserProfileActivityTest{
         intending(hasComponent(HomePageActivity::class.qualifiedName))
     }
 
+
+    @Test
+    fun test_on_clickUpdateProfile_Empty_firstName(){
+        onView(withId(R.id.et_firstName_edit_user)).perform(ViewActions.replaceText(""))
+        onView(withId(R.id.buttonUpdateProfile)).perform(ViewActions.click())
+        onView(withId(R.id.et_firstName_edit_user)).check(matches(hasErrorText("Please enter your first name")))
+    }
+
+    @Test
+    fun test_on_clickUpdateProfile_Empty_lastName(){
+        onView(withId(R.id.et_firstName_edit_user)).perform(ViewActions.replaceText("Test"))
+        onView(withId(R.id.et_lastName_edit_user)).perform(ViewActions.replaceText(""))
+        onView(withId(R.id.buttonUpdateProfile)).perform(ViewActions.click())
+        onView(withId(R.id.et_lastName_edit_user)).check(matches(hasErrorText("Please enter your last name")))
+    }
+
+    @Test
+    fun test_on_clickUpdateProfile_save(){
+        onView(withId(R.id.et_firstName_edit_user)).perform(ViewActions.replaceText("Name"))
+        onView(withId(R.id.et_lastName_edit_user)).perform(ViewActions.replaceText("Surname"))
+        onView(withId(R.id.buttonUpdateProfile)).perform(ViewActions.click())
+        intending(hasComponent(HomePageActivity::class.qualifiedName))
+    }
+
     @After
     fun tearDown(){
         Intents.release()
     }
+
+
+
 
 }
