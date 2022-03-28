d48a2b5a8bb8e87649ac4eebba75846589a992ae
anupr <anupr@DESKTOP-L0VCNF2>
2022-03-28 16:33:14 +0100

    Add comments for ForgotPasswordActivity

--- app/src/main/java/com/example/backstreet_cycles/ui/views/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/backstreet_cycles/ui/views/ForgotPasswordActivity.kt
@@ -9,18 +9,24 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 @AndroidEntryPoint
-class ForgotPasswordActivity  : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     private val forgotPasswordViewModel: ForgotPasswordViewModel by viewModels()
 
+    /**
+     * Initialise the contents within the display of the ForgotPasswordPage
+     * @param savedInstanceState used to restore a saved state so activity can be recreated
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
         initListener()
-
     }
 
+    /**
+     * Send link to reset password if an email was provided
+     */
     private fun initListener() {
         forgot_password_SendPasswordReset_button.setOnClickListener {
             forgot_password_SendPasswordReset_button.setOnClickListener {
