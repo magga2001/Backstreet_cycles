2d07ecd983965c539602b12acf054630c9cb8f1b
Vandad Vafai <vafai.vandad@gmail.com>
2022-03-28 19:13:26 +0100

    fix ChangePasswordActivityTest.kt file

--- app/src/androidTest/java/com/example/backstreet_cycles/ui/views/NavMenuTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/ui/views/NavMenuTest.kt
@@ -1,109 +1,143 @@
-//package com.example.backstreet_cycles.ui.views
-//
-//import android.app.Application
-//import androidx.test.core.app.ActivityScenario
-//import androidx.test.core.app.ApplicationProvider.getApplicationContext
-//import androidx.test.espresso.Espresso.*
-//import androidx.test.espresso.action.ViewActions.click
-//import androidx.test.espresso.assertion.ViewAssertions.matches
-//import androidx.test.espresso.matcher.ViewMatchers.*
-//import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-//import com.example.backstreet_cycles.R
-//import com.example.backstreet_cycles.domain.model.dto.Users
-//import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.firestore.FirebaseFirestore
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//
-//
-//@RunWith(AndroidJUnit4ClassRunner::class)
-//class NavMenuTest {
-//
-//    @Before
-//    fun setUp() {
-//        ActivityScenario.launch(HomePageActivity::class.java)
-//        onView(withContentDescription(R.string.open)).perform(click())
-//    }
-//
-//    @Test
-//    fun test_drawer_is_open(){
-//        onView(withId(R.id.homepage_navView)).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun test_drawer_is_closed(){
-//        onView(withContentDescription(R.string.close)).perform(click())
-//        onView(withId(R.id.homepage_navView)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
-//    }
-//
-//    @Test
-//    fun test_navToggle_toNavMenu() {
-//        onView(withId(R.id.profile)).check(matches(isDisplayed()))
-//        onView(withId(R.id.changePassword)).check(matches(isDisplayed()))
-//        onView(withId(R.id.about)).check(matches(isDisplayed()))
-//        onView(withId(R.id.faq)).check(matches(isDisplayed()))
-//        onView(withId(R.id.logout)).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun test_viewProfileButton_toEditProfileActivity() {
-//        onView(withId(R.id.profile)).perform(click())
-//        onView(withId(R.id.editUserProfile)).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun test_changePassword_toChangeEmailOrPasswordActivity() {
-//        onView(withId(R.id.changePassword)).perform(click())
-//        onView(withId(R.id.changeEmailOrPassword)).check(matches(isDisplayed()))
-//    }
-//
-//    /*@Test
-//    fun test_aboutButton_???()
-//    {
-//        val activityScenario = ActivityScenario.launch(HomePageActivity::class.java)
-//        onView(withId(R.id.nav_view)).perform(click())
-//        onView(withId(R.id.about)).perform(ViewActions.click())
-//        onView(withId(R.id.aboutActivity)).check(matches(isDisplayed()))
-//    }
-//     */
-//
-//    @Test
-//    fun test_nav_showUserName(){
-//
-//        onView(withId(R.id.user_name)).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun test_nav_showUserEmail(){
-//
-//        onView(withId(R.id.nav_header_textView_email)).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun test_nav_equalCurrentUserName(){
-//
-//        val testUserName = FirebaseFirestore.getInstance().collection("users")
-//            .whereEqualTo("email", FirebaseAuth.getInstance().currentUser!!.email)
-//            .get().result.toObjects(Users::class.java)[0].firstName
-//
-//        val textElement = "Hello: $testUserName"
-//
-//        val testDisplayedName = getApplicationContext<Application>().getString(R.id.user_name)
-//
-//
-//        assert(testDisplayedName == textElement)
-//    }
-//
-////    @Test
-////    fun test_nav_equalCurrentUserEmail(){
-////        setContentView(R.layout.activity_homepage)
-////        val email = FirebaseAuth.getInstance().currentUser?.email
-////
-////
-////        getApplicationContext<Application>().get(R.id.tv_email).text
-////
-////        onView(withId(R.id.tv_email)).check(matches(withText(email)))
-////        //assert(testDisplayedEmail == email)
-////    }
-//}
+package com.example.backstreet_cycles.ui.views
+
+import android.app.Application
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.example.backstreet_cycles.R
+import com.example.backstreet_cycles.domain.model.dto.Users
+import com.example.backstreet_cycles.ui.viewModel.LogInViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.com.example.backstreet_cycles.FakeCyclistRepoImpl
+import java.com.example.backstreet_cycles.FakeMapboxRepoImpl
+import java.com.example.backstreet_cycles.FakeTflRepoImpl
+import java.com.example.backstreet_cycles.FakeUserRepoImpl
+
+
+@HiltAndroidTest
+class NavMenuTest{
+
+    private val email = "backstreet.cycles.test.user@gmail.com"
+    private val password = "123456"
+
+    private val fakeUserRepoImpl = FakeUserRepoImpl()
+    private val fakeTflRepoImpl = FakeTflRepoImpl()
+    private val fakeMapboxRepoImpl = FakeMapboxRepoImpl()
+    private val fakeCyclistRepoImpl = FakeCyclistRepoImpl()
+    private val context = Application()
+
+    private val logInViewModel = LogInViewModel(fakeTflRepoImpl,fakeMapboxRepoImpl,fakeCyclistRepoImpl,fakeUserRepoImpl,context)
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<HomePageActivity> =
+        ActivityScenarioRule(HomePageActivity::class.java)
+
+    @get:Rule
+    val locationRule: GrantPermissionRule =
+        GrantPermissionRule.grant(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_NETWORK_STATE,
+            android.Manifest.permission.INTERNET)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        if (logInViewModel.getMutableLiveData().value != null){
+            fakeUserRepoImpl.logout()
+        }
+        Intents.init()
+        onView(withContentDescription(R.string.open)).perform(click())
+    }
+
+    @Test
+    fun test_drawer_is_open(){
+        onView(withId(R.id.nav_Menu)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_drawer_is_closed(){
+        onView(withContentDescription(R.string.close)).perform(click())
+        onView(withId(R.id.nav_Menu)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
+    }
+
+    @Test
+    fun test_navToggle_toNavMenu() {
+        onView(withId(R.id.profile)).check(matches(isDisplayed()))
+        onView(withId(R.id.changePassword)).check(matches(isDisplayed()))
+        onView(withId(R.id.about)).check(matches(isDisplayed()))
+        onView(withId(R.id.faq)).check(matches(isDisplayed()))
+        onView(withId(R.id.logout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_viewProfileButton_toEditProfileActivity() {
+        onView(withId(R.id.profile)).perform(click())
+        onView(withId(R.id.editUserProfileActivity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_changePassword_toChangeEmailOrPasswordActivity() {
+        onView(withId(R.id.changePassword)).perform(click())
+        onView(withId(R.id.changePasswordActivity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_aboutButton(){
+        onView(withId(R.id.about)).perform(click())
+        onView(withId(R.id.aboutActivity)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun test_nav_showUserName(){
+        onView(withId(R.id.user_name)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_nav_showUserEmail(){
+        onView(withId(R.id.nav_header_textView_email)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_nav_equalCurrentUserName(){
+
+        val testUserName = FirebaseFirestore.getInstance().collection("users")
+            .whereEqualTo("email", FirebaseAuth.getInstance().currentUser!!.email)
+            .get().result.toObjects(Users::class.java)[0].firstName
+
+        val textElement = "Hello: $testUserName"
+
+        val testDisplayedName = getApplicationContext<Application>().getString(R.id.user_name)
+
+
+        assert(testDisplayedName == textElement)
+    }
+
+    @Test
+    fun test_nav_equalCurrentUserEmail(){
+        //setContentView(R.layout.activity_homepage)
+        val email = FirebaseAuth.getInstance().currentUser?.email
+
+        //val testDisplayedEmail = onView(withId(R.id.nav_header_textView_email)).toString()
+        //getApplicationContext<Application>().onView(R.id.tv_email).text
+
+        onView(withId(R.id.nav_header_textView_email)).check(matches(withText(email)))
+        //assert(testDisplayedEmail == email)
+    }
+}
