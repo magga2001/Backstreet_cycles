11cf016f2f8269a94f4f25bb4e5722a05fb40d74
pkur3k <74495381+pkur3k@users.noreply.github.com>
2022-03-29 15:36:34 +0100

    Add new tests to JourneyActivityTest and correct old ones

--- app/src/androidTest/java/com/example/backstreet_cycles/ui/views/JourneyActivityTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/ui/views/JourneyActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.backstreet_cycles.ui.views
 
 import android.app.Application
+import android.os.SystemClock.sleep
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
@@ -9,6 +10,7 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.init
 import androidx.test.espresso.intent.Intents.intending
@@ -19,10 +21,12 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.GrantPermissionRule
 import com.example.backstreet_cycles.R
+import com.example.backstreet_cycles.domain.adapter.PlanJourneyAdapter
 import com.example.backstreet_cycles.ui.viewModel.LogInViewModel
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.core.IsNot.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -52,8 +56,8 @@ class JourneyActivityTest{
     var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
-    var activityRule: ActivityScenarioRule<JourneyActivity> =
-        ActivityScenarioRule(JourneyActivity::class.java)
+    var activityRule: ActivityScenarioRule<HomePageActivity> =
+        ActivityScenarioRule(HomePageActivity::class.java)
 
     @get:Rule
     val locationRule: GrantPermissionRule =
@@ -69,8 +73,7 @@ class JourneyActivityTest{
             fakeUserRepoImpl.login(email, password)
         }
         Intents.init()
-        /*ActivityScenario.launch(HomePageActivity::class.java)
-        onView(withId(R.id.nextPageButton)).perform(click())*/
+        onView(withId(R.id.nextPageButton)).perform(click())
         ActivityScenario.launch(JourneyActivity::class.java)
     }
 
@@ -102,29 +105,35 @@ class JourneyActivityTest{
 
     @Test
     fun test_stops_recycling_view_displayed(){
-//        onView(withId(R.id.plan_journey_recycling_view)).check(matches(isDisplayed()))
         onView(withId(R.id.plan_journey_recycling_view)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
-    /*@Test
-       fun test_journey_overview_clicked(){
-            onView(withId(R.id.journey_activity)).check(matches(isDisplayed()))
-        }*/
-
     @Test
-    fun test_start_navigation_clicked_goes_to_navigation_activity(){
-        onView(withId(R.id.start_navigation)).perform(click())
-        onView(withId(R.id.navigation_layout)).check(matches(isDisplayed()))
-    }
+    fun test_journey_overview_is_clickable(){
+        onView(withId(R.id.overview_journey)).check(matches(isClickable()))
 
-    @Test
-    fun test_expand_button(){
-        onView(withId(R.id.planJourney_button_expand)).check(matches(isDisplayed()))
     }
 
 //    @Test
+//    fun test_journey_overview_is_clicked(){
+//        onView(withId(R.id.overview_journey)).perform(click())
+//        //intending(hasComponent(JourneyActivity::class.qualifiedName))
+//    }
+
+//    @Test
+//    fun test_start_navigation_clicked_goes_to_navigation_activity(){
+//        onView(withId(R.id.start_navigation)).perform(click())
+//        onView(withId(R.id.navigation_layout)).check(matches(isDisplayed()))
+//    }
+
+//    @Test
+//    fun test_expand_button(){
+//        onView(withId(R.id.planJourney_button_expand)).check(matches(isDisplayed()))
+//    }
+
+//    @Test
 //    fun test_button_expand() {
-//        onView(withId(R.id.bottom_sheet_view_journey)).check(matches(isDisplayed()))
+//        onView(withId(R.id.journey_bottom_sheet_view)).check(matches(isDisplayed()))
 //        onView(withId(R.id.plan_journey_recycling_view)).check(matches(isDisplayed()))
 //        onView(withId(R.id.plan_journey_recycling_view)).perform(RecyclerViewActions.scrollToPosition<PlanJourneyAdapter.ViewHolder>(0))
 //
@@ -134,25 +143,25 @@ class JourneyActivityTest{
 //        onView(withId(R.id.setNav1)).check(matches(isDisplayed()))
 //        onView(withId(R.id.setNav2)).check(matches(isDisplayed()))
 //        onView(withId(R.id.setNav3)).check(matches(isDisplayed()))
+
+
+
+        //
+        //        onView(withId(R.id.bottom_sheet_journey)).perform(ViewActions.swipeUp())
+        //        onView(withId(R.id.button_expand)).check(matches(isDisplayed()))
+    //}
+
+
+//    @Test
+//    fun test_stop_clicked_set_navigation_displayed(){
+//        onView(withId(R.id.bottom_sheet_journey)).check(matches(isDisplayed()))
+//        onView(withId(R.id.bottom_sheet_journey)).perform(scrollTo())
+//        //onView(withId(R.id.button_expand)).perform(ViewActions.)
+//        onView(withId(R.id.setNav1)).check(matches(isDisplayed()))
+//        onView(withId(R.id.setNav2)).check(matches(isDisplayed()))
+//        onView(withId(R.id.setNav3)).check(matches(isDisplayed()))
 //
-//
-//
-//        //
-//        //        onView(withId(R.id.bottom_sheet_journey)).perform(ViewActions.swipeUp())
-//        //        onView(withId(R.id.button_expand)).check(matches(isDisplayed()))
 //    }
-
-
-    @Test
-    fun test_stop_clicked_set_navigation_displayed(){
-        onView(withId(R.id.bottom_sheet_journey)).check(matches(isDisplayed()))
-        onView(withId(R.id.bottom_sheet_journey)).perform(scrollTo())
-        //onView(withId(R.id.button_expand)).perform(ViewActions.)
-        onView(withId(R.id.setNav1)).check(matches(isDisplayed()))
-        onView(withId(R.id.setNav2)).check(matches(isDisplayed()))
-        onView(withId(R.id.setNav3)).check(matches(isDisplayed()))
-
-    }
 
 //    @Test
 //    fun test_stop_clicked_images_displayed(){
@@ -161,11 +170,11 @@ class JourneyActivityTest{
 //        onView(withId(R.id.imageView14)).check(matches(isDisplayed()))
 //    }
 
-    @Test
-    fun test_set_navigation_clicked(){
-
-        onView(withId(R.id.journeyActivity)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun test_set_navigation_displayed(){
+//
+//        onView(withId(R.id.journeyActivity)).check(matches(isDisplayed()))
+//    }
 
 //    fun viewExists(id: Int): Boolean {
 //        return try {
@@ -178,9 +187,7 @@ class JourneyActivityTest{
 
     @Test
     fun test_hire_button_visible(){
-        //onView(withId(R.id.santander_link)).check(matches(isDisplayed()))
         onView(withId(R.id.santander_link)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-
     }
 
     @Test
@@ -188,15 +195,15 @@ class JourneyActivityTest{
         onView(withId(R.id.SantanderCycleImage)).check(matches(isDisplayed()))
     }
 
-    @Test
-    fun test_from_text(){
-        onView(withId(R.id.planJourney_from)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun test_from_text(){
+//        onView(withId(R.id.planJourney_from)).check(matches(isDisplayed()))
+//    }
 
-    @Test
-    fun test_to_text(){
-        onView(withId(R.id.planJourney_to)).check(matches(isDisplayed()))
-    }
+//    @Test
+//    fun test_to_text(){
+//        onView(withId(R.id.planJourney_to)).check(matches(isDisplayed()))
+//    }
 
     @Test
     fun test_finish_journey_button_visible(){
@@ -205,12 +212,12 @@ class JourneyActivityTest{
         onView(withId(R.id.finish_journey)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
-    @Test
-    fun test_checkbox_visible(){
-        //onView(withId(R.id.checkBoxFinishJourney)).check(matches(isDisplayed()))
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-
-    }
+//    @Test
+//    fun test_checkbox_visible(){
+//        //onView(withId(R.id.checkBoxFinishJourney)).check(matches(isDisplayed()))
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+//
+//    }
 
     @Test
     fun test_duration_text_field_displayed(){
@@ -236,55 +243,64 @@ class JourneyActivityTest{
 
     @Test
     fun test_duration_image_field_displayed(){
-        onView(withId(R.id.duration_image)).check(matches(isDisplayed()))
+        onView(withId(R.id.duration_image)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
     @Test
     fun test_distance_image_field_displayed(){
-        onView(withId(R.id.DistanceImage)).check(matches(isDisplayed()))
+        onView(withId(R.id.DistanceImage)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
     @Test
     fun test_price_image_field_displayed(){
-        onView(withId(R.id.price_image)).check(matches(isDisplayed()))
+        onView(withId(R.id.price_image)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
+//    @Test
+//    fun test_check_not_checked(){
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
+//    }
+
+//    @Test
+//    fun test_journey_nav_not_clickable_if_not_set(){
+//
+//    }
+
     @Test
-    fun test_check_not_checked(){
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
+    fun test_finish_button_visible(){
+        onView(withId(R.id.finish_journey)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 
     @Test
     fun test_finish_not_clickable(){
-        onView(withId(R.id.bottom_sheet_journey)).perform(scrollTo(), click())
-        onView(withId(R.id.finish_journey)).check(matches(isNotClickable()))
+        onView(withId(R.id.finish_journey)).check(matches(not(isEnabled())));
     }
 
-    @Test
-    fun test_not_checked_stop_clickable(){
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
-        onView(withId(R.id.planJourney_button_expand)).check(matches(isClickable()))
-    }
+//    @Test
+//    fun test_not_checked_stop_clickable(){
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
+//        onView(withId(R.id.planJourney_button_expand)).check(matches(isClickable()))
+//    }
 
-    @Test
-    fun test_not_checked_finish_not_clickable(){
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
-        onView(withId(R.id.finish_journey)).check(matches(isNotClickable()))
-    }
+//    @Test
+//    fun test_not_checked_finish_not_clickable(){
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isNotChecked()))
+//        onView(withId(R.id.finish_journey)).check(matches(isNotClickable()))
+//    }
 
-    @Test
-    fun test_checked_stop_not_clickable(){
-        onView(withId(R.id.checkBoxFinishJourney)).perform(click())
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isChecked()))
-        onView(withId(R.id.planJourney_button_expand)).check(matches(isNotClickable()))
-    }
+//    @Test
+//    fun test_checked_stop_not_clickable(){
+//        onView(withId(R.id.checkBoxFinishJourney)).perform(click())
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isChecked()))
+//        onView(withId(R.id.planJourney_button_expand)).check(matches(isNotClickable()))
+//    }
 
-    @Test
-    fun test_checked_finish_clickable(){
-        onView(withId(R.id.checkBoxFinishJourney)).perform(click())
-        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isChecked()))
-        onView(withId(R.id.finish_journey)).check(matches(isClickable()));
-    }
+//    @Test
+//    fun test_checked_finish_clickable(){
+//        onView(withId(R.id.checkBoxFinishJourney)).perform(click())
+//        onView(withId(R.id.checkBoxFinishJourney)).check(matches(isChecked()))
+//        onView(withId(R.id.finish_journey)).check(matches(isClickable()));
+//    }
 
     @After
     fun tearDown(){
