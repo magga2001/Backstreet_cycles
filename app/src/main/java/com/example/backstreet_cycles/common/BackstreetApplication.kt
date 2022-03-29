8698b1b7d839733a21945b2eefdafe4ae8f011b3
pkur3k <74495381+pkur3k@users.noreply.github.com>
2022-03-29 13:05:47 +0100

    Disable DarkMode

--- app/src/main/java/com/example/backstreet_cycles/common/BackstreetApplication.kt
+++ app/src/main/java/com/example/backstreet_cycles/common/BackstreetApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 //import androidx.hilt.work.HiltWorkerFactory
@@ -26,6 +27,8 @@ class BackstreetApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         SharedPrefHelper.initialiseSharedPref(this, Constants.DOCKS_LOCATIONS)
 
