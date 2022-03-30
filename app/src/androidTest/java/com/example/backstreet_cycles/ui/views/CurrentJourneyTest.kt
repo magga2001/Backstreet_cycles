2c533d85c02663d4c6b9630ff4ff516e8c8c4baa
pkur3k <74495381+pkur3k@users.noreply.github.com>
2022-03-30 13:23:33 +0100

    Change CurrentJourneyTest

--- app/src/androidTest/java/com/example/backstreet_cycles/ui/views/CurrentJourneyTest.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/ui/views/CurrentJourneyTest.kt
@@ -1,45 +1,77 @@
 package com.example.backstreet_cycles.ui.views
 
-//import android.app.Application
-//import androidx.test.core.app.ActivityScenario
-//import androidx.test.espresso.intent.Intents
-//import androidx.test.espresso.intent.Intents.init
-//import androidx.test.espresso.intent.Intents.intending
-//import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-//import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-//import org.junit.After
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//
-//@RunWith(AndroidJUnit4ClassRunner::class)
-//class CurrentJourneyTest {
-//
-//    lateinit var logInRegisterViewModel: LogInRegisterViewModel
-//
-//    @Before
-//    fun setUp() {
-//        logInRegisterViewModel = LogInRegisterViewModel(Application())
-//        logInRegisterViewModel.login("backstreet.cycles.test.user@gmail.com", "123456")
-//        ActivityScenario.launch(JourneyActivity::class.java)
-//        init()
-//    }
-//
-//    @Test
-//    fun test_activity_is_in_view() {
-//        intending(hasComponent(JourneyActivity::class.qualifiedName))
-//
-//    }
-//
-////    @Test
-////    fun test_on_pressBack_go_to_HomePageActivity() {
-////        Espresso.pressBack()
-////        intending(hasComponent(HomePageActivity::class.qualifiedName))
-////
-////    }
-//
-//    @After
-//    fun tearDown(){
-//        Intents.release()
-//    }
-//}
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.GrantPermissionRule
+import com.example.backstreet_cycles.R
+import com.example.backstreet_cycles.common.EspressoIdlingResource
+import com.example.backstreet_cycles.data.repository.UserRepositoryImpl
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+@HiltAndroidTest
+class CurrentJourneyTest{
+
+    private val email = "backstreet.cycles.test.user@gmail.com"
+    private val password = "123456"
+
+    private val userRepoImpl = UserRepositoryImpl()
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val locationRule: GrantPermissionRule =
+        GrantPermissionRule.grant(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_NETWORK_STATE,
+            android.Manifest.permission.INTERNET)
+
+    @Before
+    fun setUp() {
+        if(userRepoImpl.getFirebaseAuthUser() == null){
+            userRepoImpl.login(email, password)
+        }
+        hiltRule.inject()
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        ActivityScenario.launch(HomePageActivity::class.java)
+        Espresso.onView(ViewMatchers.withContentDescription(R.string.open)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.currentJourney)).perform(ViewActions.click())
+    }
+
+    @Test
+    fun test_activity_is_in_view() {
+        Intents.init()
+        intending(hasComponent(JourneyActivity::class.qualifiedName))
+        Intents.release()
+    }
+
+    @Test
+    fun test_on_pressBack_go_to_HomePageActivity() {
+        Espresso.pressBack()
+        Intents.init()
+        intending(hasComponent(HomePageActivity::class.qualifiedName))
+        Intents.release()
+    }
+
+    @After
+    fun tearDown(){
+        if(userRepoImpl.getFirebaseAuthUser() != null){
+            IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+            userRepoImpl.logout()
+        }
+    }
+}
