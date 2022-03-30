b833a45809abc3390f1f88e653b00b83d9eed415
Shahmir Malik <k19029862@kcl.ac.uk>
2022-03-30 19:03:45 +0100

    Merge version2-testing

--- app/src/androidTest/java/com/example/backstreet_cycles/BackstreetTestRunner.kt
+++ app/src/androidTest/java/com/example/backstreet_cycles/BackstreetTestRunner.kt
@@ -1,17 +1,17 @@
-//package com.example.backstreet_cycles
-//
-//import android.app.Application
-//import android.content.Context
-//import androidx.test.runner.AndroidJUnitRunner
-//import dagger.hilt.android.testing.HiltTestApplication
-//
-//class BackstreetTestRunner : AndroidJUnitRunner() {
-//
-//    override fun newApplication(
-//        cl: ClassLoader?,
-//        className: String?,
-//        context: Context?
-//    ): Application {
-//        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
-//    }
-//}
+package com.example.backstreet_cycles
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class BackstreetTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
