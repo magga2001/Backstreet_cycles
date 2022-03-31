8e9f3db80bd3145bd9846102e8192715944b6f0f
Shahmir Malik <k19029862@kcl.ac.uk>
2022-03-31 18:37:18 +0100

    Fix no matching file error

---
+++ app/src/androidTest/java/com/example/backstreet_cycles/ui/viewModels/JourneyViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.example.backstreet_cycles.ui.viewModels
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.example.backstreet_cycles.FakeCyclistRepoImpl
+import com.example.backstreet_cycles.FakeMapboxRepoImpl
+import com.example.backstreet_cycles.FakeTflRepoImpl
+import com.example.backstreet_cycles.FakeUserRepoImpl
+import com.example.backstreet_cycles.ui.viewModel.BaseViewModel
+import com.example.backstreet_cycles.ui.viewModel.JourneyViewModel
+import dagger.hilt.android.internal.Contexts
+import junit.framework.TestCase
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class JourneyViewModelTest {
+
+    private lateinit var journeyViewModel: JourneyViewModel
+    lateinit var instrumentationContext: Context
+
+//
+//    @get:Rule
+//    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setUp()
+    {
+//        hiltRule.inject()
+//        val context = mockk<Context>(relaxed = true)
+//        val application = mockk<BackstreetApplication>(relaxed = true)
+        instrumentationContext = ApplicationProvider.getApplicationContext();
+        val application = Contexts.getApplication(instrumentationContext)
+        journeyViewModel = JourneyViewModel(
+            FakeTflRepoImpl(),
+            FakeMapboxRepoImpl(),
+            FakeCyclistRepoImpl(),
+            FakeUserRepoImpl(),
+            application,
+            instrumentationContext
+        )
+    }
+}
