aaae1542905a0bc0ab902653ddf7f9d2295b60c6
Rany-Touffaha <rany.touffaha@kcl.ac.uk>
2022-04-02 20:59:58 +0100

    Add local cache files for docks in case tfl API fails

--- app/src/main/java/com/example/backstreet_cycles/ui/viewModel/SplashScreenViewModel.kt
+++ app/src/main/java/com/example/backstreet_cycles/ui/viewModel/SplashScreenViewModel.kt
@@ -5,9 +5,10 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.backstreet_cycles.common.BackstreetApplication
 import com.example.backstreet_cycles.common.Resource
+import com.example.backstreet_cycles.domain.model.dto.Dock
 import com.example.backstreet_cycles.domain.repositoryInt.*
+import com.example.backstreet_cycles.domain.utils.JsonHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.launchIn
@@ -41,7 +42,10 @@ class SplashScreenViewModel @Inject constructor(
                     Log.i("Splash screen dock", result.data?.size.toString())
 
                     if (result.data != null && result.data.isNotEmpty()) {
-                        tflRepository.setCurrentDocks(result.data!!)
+                        tflRepository.setCurrentDocks(result.data)
+                        val dockJSON = JsonHelper.objectToString(result.data, Dock::class.java)
+                        JsonHelper.writeJsonFile(mContext, "localdocks.json", dockJSON)
+                        Log.i("Splash screen dock", JsonHelper.readJsonFile(mContext, "localdocks.json").toString())
                     }
                     loadTouristAttractions()
                 }
