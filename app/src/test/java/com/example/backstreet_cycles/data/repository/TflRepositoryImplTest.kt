353e7ba53d04df486efd578e131a156c8b8edc1a
Vandad Vafai <vafai.vandad@gmail.com>
2022-04-03 13:24:59 +0100

    Move files for the unit tests to work again.

--- app/src/test/java/com/example/backstreet_cycles/data/repository/TflRepositoryImplTest.kt
+++ app/src/test/java/com/example/backstreet_cycles/data/repository/TflRepositoryImplTest.kt
@@ -1,55 +1,55 @@
 package com.example.backstreet_cycles.data.repository
-//
-//import com.example.backstreet_cycles.common.Constants
-//import com.example.backstreet_cycles.data.remote.TflApi
-//import com.example.backstreet_cycles.domain.model.dto.Dock
-//import com.squareup.moshi.Moshi
-//import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import org.junit.runners.JUnit4
-//import org.mockito.Mock
-//import retrofit2.Retrofit
-//import retrofit2.converter.moshi.MoshiConverterFactory
-//
-//@RunWith(JUnit4::class)
-//class TflRepositoryImplTest{
-//
-//    private lateinit var tflRepositoryImpl: TflRepositoryImpl
-//
-//    @Mock
-//    private lateinit var tflApi: TflApi
-//
-//    @Before
-//    fun setUp(){
-//        val adapterFactory = KotlinJsonAdapterFactory()
-//        val moshi = Moshi.Builder().add(adapterFactory).build()
-//        val moshiFactory = MoshiConverterFactory.create(moshi)
-//
-//        tflApi = Retrofit.Builder()
-//            .baseUrl(Constants.BASE_URL)
-//            .addConverterFactory(moshiFactory)
-//            .build()
-//            .create(TflApi::class.java)
-//        tflRepositoryImpl = TflRepositoryImpl(tflApi)
-//    }
-//
-//    @Test
-//    fun `test if you can set the current Docks`(){
-//        val docks = mutableListOf(
-//            Dock("1","Test_1",0.9),
-//            Dock("2","Test_2",0.9)
-//        )
-//        tflRepositoryImpl.setCurrentDocks(docks)
-//        assert(tflRepositoryImpl.getCurrentDocks().isNotEmpty())
-//    }
-//
-//    @Test
-//    fun `test if you can get the current Docks`(){
-//        assert(tflRepositoryImpl.getCurrentDocks().isEmpty())
-//    }
-//
-//
-//
-//}
+
+import com.example.backstreet_cycles.common.Constants
+import com.example.backstreet_cycles.data.remote.TflApi
+import com.example.backstreet_cycles.domain.model.dto.Dock
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@RunWith(JUnit4::class)
+class TflRepositoryImplTest{
+
+    private lateinit var tflRepositoryImpl: TflRepositoryImpl
+
+    @Mock
+    private lateinit var tflApi: TflApi
+
+    @Before
+    fun setUp(){
+        val adapterFactory = KotlinJsonAdapterFactory()
+        val moshi = Moshi.Builder().add(adapterFactory).build()
+        val moshiFactory = MoshiConverterFactory.create(moshi)
+
+        tflApi = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(moshiFactory)
+            .build()
+            .create(TflApi::class.java)
+        tflRepositoryImpl = TflRepositoryImpl(tflApi)
+    }
+
+    @Test
+    fun `test if you can set the current Docks`(){
+        val docks = mutableListOf(
+            Dock("1","Test_1",0.9),
+            Dock("2","Test_2",0.9)
+        )
+        tflRepositoryImpl.setCurrentDocks(docks)
+        assert(tflRepositoryImpl.getCurrentDocks().isNotEmpty())
+    }
+
+    @Test
+    fun `test if you can get the current Docks`(){
+        assert(tflRepositoryImpl.getCurrentDocks().isEmpty())
+    }
+
+
+
+}
--- app/src/test/java/com/example/backstreet_cycles/domain/utils/JsonHelperTest.kt
+++ app/src/test/java/com/example/backstreet_cycles/domain/utils/JsonHelperTest.kt
@@ -1,36 +1,36 @@
 package com.example.backstreet_cycles.domain.utils
 
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class JsonHelperTest{
 //
-//import org.junit.Assert.assertEquals
-//import org.junit.Test
-//
-//
-//class JsonHelperTest{
-////
-////    @Test
-////    fun test_string_to_object(){
-////
-////        val testList: List<Locations> = listOf(
-////            Locations("Tower of London",51.5081, -0.0759 ),
-////            Locations("St Paul’s Cathedral",51.5138, -0.0984 ),
-////            Locations("Tate Modern",51.5076, -0.0994 )
-////        )
-////        val resultObject = JsonHelper.stringToObject(testList.toString(), Locations::class.java)
-////        assertEquals(resultObject, testList)
-////    }
-////
 //    @Test
-//    fun test_object_to_string(){
-//        val numOfCyclists = listOf<String>("3","4")
-//        val resultString = JsonHelper.objectToString(numOfCyclists,String::class.java)
-//        val expectedString = "[\"3\",\"4\"]"
-//        assertEquals(resultString, expectedString)
+//    fun test_string_to_object(){
+//
+//        val testList: List<Locations> = listOf(
+//            Locations("Tower of London",51.5081, -0.0759 ),
+//            Locations("St Paul’s Cathedral",51.5138, -0.0984 ),
+//            Locations("Tate Modern",51.5076, -0.0994 )
+//        )
+//        val resultObject = JsonHelper.stringToObject(testList.toString(), Locations::class.java)
+//        assertEquals(resultObject, testList)
 //    }
-////
-//////    @Test
-//////    fun test_get_Json_from_resources(){
-//////        val application = Application()
-//////        val result = JsonHelper.getJsonFromResources(application, R.raw.touristattraction)
-//////        assertEquals(result, R.raw.touristattraction.toString())
-//////    }
-//}
+//
+    @Test
+    fun test_object_to_string(){
+        val numOfCyclists = listOf<String>("3","4")
+        val resultString = JsonHelper.objectToString(numOfCyclists,String::class.java)
+        val expectedString = "[\"3\",\"4\"]"
+        assertEquals(resultString, expectedString)
+    }
+//
+////    @Test
+////    fun test_get_Json_from_resources(){
+////        val application = Application()
+////        val result = JsonHelper.getJsonFromResources(application, R.raw.touristattraction)
+////        assertEquals(result, R.raw.touristattraction.toString())
+////    }
+}
